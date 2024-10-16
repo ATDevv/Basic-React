8434ad7c8e26bca015049a28892fc585b6d25626
Tuấngg Anhhh <macofanh@Tuongg-Anhh.local>
2024-10-17 00:27:52 +0700

    json stringify

--- src/Components/MyComponent.jsx
+++ src/Components/MyComponent.jsx
@@ -1,8 +1,23 @@
 
 
 const MyComponent = () => {
-    return(
-        <h1>Hi, I'm ATD</h1>
+
+    const a = 'halo'
+    const b = 25
+    const c = [1, 2, 3]
+    const d = {
+        name: 'atd',
+        age: 25
+    }
+
+    return (
+        <>
+            <h1>Hi, I'm ATD</h1>
+            <p>{a} - {b} - {JSON.stringify(c)} - {JSON.stringify(d)}</p>
+
+            //Code JS in React: use {}
+            <div>{console.log('Hello World!')}</div>
+        </>
     )
 }
 
