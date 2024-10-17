dc8d889014c71f0383e8d30a58b4b3c4f8519283
Tuấngg Anhhh <macofanh@Tuongg-Anhh.local>
2024-10-17 16:54:28 +0700

    add some domEvents

--- src/Components/todo/TodoNew.jsx
+++ src/Components/todo/TodoNew.jsx
@@ -7,10 +7,23 @@ const TodoNew = (props) => {
     // gọi hàm từ hàm con kp hàm cha
     // props.addNewTodo()
 
+    const handleClick = () => {
+        alert('Click me!')
+    }
+
+    const handleOnchange = (e) => {
+        console.log('Handel onChange', e.target.value);
+    }
+
     return (
         <div className='todo-new'>
-            <input type="text" />
-            <button>Add</button>
+            <input type="text" 
+                onChange={handleOnchange}
+            />
+            <button 
+                style={{cursor: "pointer"}}
+                onClick={handleClick}
+            >Add</button>
         </div>
     )
 }
