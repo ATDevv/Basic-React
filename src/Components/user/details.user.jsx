ecafc408d44b09a975c0e9fd0ba28a28990c967a
Tuấngg Anhhh <macofanh@Tuongg-Anhh.local>
2024-11-24 14:56:34 +0700

    display avatar

--- src/Components/user/details.user.jsx
+++ src/Components/user/details.user.jsx
@@ -1,4 +1,5 @@
-import { Button, Drawer } from 'antd'
+import { Button, Drawer, Form, Input } from 'antd'
+import FormItem from 'antd/es/form/FormItem';
 
 const ViewDetailUer = (props) => {
     const {
@@ -11,7 +12,12 @@ const ViewDetailUer = (props) => {
     }
 
     return (
-        <Drawer onClose={closeDetails} open={openDetail} title="Details User">
+        <Drawer 
+            onClose={closeDetails} 
+            open={openDetail} 
+            title="Details User"
+            width={'25vw'}
+        >
             {
                 detailData ? 
                 <>
@@ -22,6 +28,33 @@ const ViewDetailUer = (props) => {
                     <p>Email: {detailData.email}</p>
                     <br />
                     <p>Phone: {detailData.phone}</p>
+                    <br />
+                    <p>Avatar : </p>
+                    <div style={{
+                        marginTop: '10px',
+                        height: '100', width: '150',
+                        border: '1px solid black'
+                    }}>
+                        <img style={{width: '100%', height: '100%', objectFit: 'contain'}}
+                            src={`${import.meta.env.VITE_SOME_KEY}/images/avatar/${detailData.avatar}`} 
+                            alt="" 
+                        />
+                    </div>
+                    <div>
+                        <label htmlFor='update-ava' style={{
+                            display: 'block',
+                            marginTop: '15px',
+                            padding: '5px 10px',
+                            background: 'yellow',
+                            borderRadius: '7px',
+                            border: '1px solid black',
+                            cursor: 'pointer',
+                            width: 'fit-content'
+                        }}>
+                            Upload Avatar
+                        </label>
+                        <input type="file" name="update-ava" id="update-ava" hidden/>
+                    </div>
                 </>
                 :
                 <p>Empty Data</p>
